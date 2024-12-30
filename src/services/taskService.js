9c88b9c8be68e064f3f10fec8d6ab9f098d3d5cf
fastsaleinc <germainandresdev@gmail.com>
2024-12-29 21:27:03 -0300

    modify url api

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL; // Lee la URL de la variable de entorno
-
+const API_URL =  `${import.meta.env.VITE_API_URL}/api/tasks`; // Lee la URL de la variable de entorno
+console.log(API_URL)
 export const getTasks = async () => {
   const response = await axios.get(API_URL);
   return response.data;
